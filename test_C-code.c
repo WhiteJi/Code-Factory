50873b09755efb6ff8ab6d4d476f9e261f9c9f9c
WhiteJi <37925089+WhiteJi@users.noreply.github.com>
2020-09-21 22:51:54 +0900

    Update TestFile 1

---
+++ test_C-code.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+ int a=0;
+ int b=0;
+ int total =0;
+ int i;
+ for(i=1;i<=10;i++){
+ total =a+b;
+
+ printf("%d + %d = %d \n", a, b, total);
+ a++;
+ b++;
+
+ }
+
+ return 0;
+}
